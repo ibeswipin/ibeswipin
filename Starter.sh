830681b057ab298bb53194cc1c5606362c5e2cf5
Fazliddin <103936585+AmoreForever@users.noreply.github.com>
2022-10-02 14:23:32 +0500

    Update Starter.sh

--- Starter.sh
+++ Starter.sh
@@ -23,4 +23,4 @@ cd && sudo apt update -y && sudo apt upgrade -y
 sudo apt install python3.8 -y && sudo apt install tmux -y && sudo apt install git -y && sudo apt install python3.8-distutils -y
 
 # start userbot 
-git clone https://github.com/hikariatama/Hikka && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3.8 get-pip.py && cd Hikka && python3.8 -m pip install -r requirements.txt
+git clone https://github.com/hikariatama/Hikka && curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py && python3.8 get-pip.py && cd Hikka && python3.8 -m pip install -r requirements.txt && (python3.8 -m hikka --port 8080 &)
